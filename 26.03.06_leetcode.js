2c30e88fbef9f47d91ab75c3a50c84d4c52c05e5
Noah <1476021453@qq.com>
2026-03-06 23:20:47 +0800

    2026.03.06力扣网刷题 897. 递增顺序搜索树——中级工程师、栈、树、深度优先搜索、二叉搜索树、二叉树、第100场周赛——简单 方法一：中序遍历

---
+++ 26.03.06_leetcode.js
@@ -0,0 +1,42 @@
+// 2026.03.06力扣网刷题
+// 897. 递增顺序搜索树——中级工程师、栈、树、深度优先搜索、二叉搜索树、二叉树、第100场周赛——简单
+// 给你一棵二叉搜索树的 root ，请你 按中序遍历 将其重新排列为一棵递增顺序搜索树，使树中最左边的节点成为树的根节点，并且每个节点没有左子节点，只有一个右子节点。
+// 示例 1：
+// 输入：root = [5, 3, 6, 2, 4, null, 8, 1, null, null, null, 7, 9]
+// 输出：[1, null, 2, null, 3, null, 4, null, 5, null, 6, null, 7, null, 8, null, 9]
+// 示例 2：
+// 输入：root = [5, 1, 7]
+// 输出：[1, null, 5, null, 7]
+// 提示：
+// 树中节点数的取值范围是[1, 100]
+// 0 <= Node.val <= 1000
+
+function TreeNode(val, left, right) {
+    this.val = val === undefined ? 0 : val;
+    this.left = left === undefined ? null : left;
+    this.right = right === undefined ? null : right;
+}
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var increasingBST = function (root) {
+    let newRoot = null,
+        pre = null;
+    function dfs(root) {
+        if (!root) {
+            return;
+        }
+        dfs(root.left);
+        if (!newRoot) {
+            newRoot = root;
+        } else {
+            pre.right = root;
+        }
+        pre = root;
+        pre.left = null;
+        dfs(root.right);
+    }
+    dfs(root);
+    return newRoot;
+};
